139749119315bd046f1e311b1750c490cddffb56
JennyPocket77 <80539307+JennyPocket77@users.noreply.github.com>
2022-03-07 21:27:03 -0700

    updated github URL

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
  <h1>React Weather Forecast App</h1>
  <footer>This project was coded by <a href="https://objective-cori-203d1a.netlify.app/index.html" target="_blank" rel="noreferrer">
- Jennifer Ann Gonzalez</a> and is <a href="https://github.com/JennyPocket77/react-forecast-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
+ Jennifer Ann Gonzalez</a> and is <a href="https://github.com/JennyPocket77/new-react-forecast-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a></footer>
     </div>
   );
 }
