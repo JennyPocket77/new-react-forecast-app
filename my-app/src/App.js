ab60da9bc8f805f634c49425858e0c0e35afcf84
JennyPocket77 <80539307+JennyPocket77@users.noreply.github.com>
2022-03-27 22:32:46 -0600

    updated open sourced and hosting links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
 <Weather defaultCity="New York" />
 
  <footer>This project was coded by <a href="https://objective-cori-203d1a.netlify.app/index.html" target="_blank" rel="noreferrer">
- Jennifer Ann Gonzalez</a> and is <a href="https://github.com/JennyPocket77/new-react-forecast-app" target="_blank" rel="noreferrer">open-sourced on GitHub
+ Jennifer Ann Gonzalez</a> and is open-sourced on <a href="https://github.com/JennyPocket77/new-react-forecast-app" target="_blank" rel="noreferrer"> GitHub</a> and hosted on <a href="https://storied-nasturtium-8926c9.netlify.app/"> Netlify 
  </a>
  </footer>
     </div>
